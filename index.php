627ed7f91a845bc88f5d0a38576a56db03fb02fb
Ubuntu-github <59200946+Ubuntu-github@users.noreply.github.com>
2019-12-24 21:24:11 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<html lang="en"><head>
+    <meta charset="UTF-8">
+    <title> Ubuntu-github-index</title>
+  </head>
+  <body>
+    <h1> first try </h1>
+    <h2> next line is language of php  (echo $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];)</h2>
+    <div> the result : <?php echo $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?> </div>
+  
+
+</body></html>
